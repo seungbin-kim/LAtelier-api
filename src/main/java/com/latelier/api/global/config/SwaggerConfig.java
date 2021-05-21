ed396127ea732ed5692e717c1be4b811789787e5
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-05-22 01:20:30 +0900

    update: swagger 3.0.0 버전으로 수정
    - swagger 3.0.0 버전으로 수정, 설정변경

--- src/main/java/com/latelier/api/global/config/SwaggerConfig.java
+++ src/main/java/com/latelier/api/global/config/SwaggerConfig.java
@@ -6,17 +6,16 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
+    return new Docket(DocumentationType.OAS_30)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("com.latelier.api.domain"))
+        .apis(RequestHandlerSelectors
+            .basePackage("com.latelier.api.domain"))
         .paths(PathSelectors.any())
         .build();
   }
