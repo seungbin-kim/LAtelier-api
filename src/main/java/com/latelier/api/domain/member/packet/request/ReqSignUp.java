238e50344b6de6bf0eaaa1a4a14603db315ba345
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-06-03 20:54:53 +0900

    update: 필드 추가
    - 회원가입 필드 추가

--- src/main/java/com/latelier/api/domain/member/packet/request/ReqSignUp.java
+++ src/main/java/com/latelier/api/domain/member/packet/request/ReqSignUp.java
@@ -5,29 +5,33 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Getter
 @EqualsAndHashCode
 @ApiModel("회원가입 요청정보")
 public class ReqSignUp {
 
+  @Size(
+      min = 2,
+      max = 10,
+      message = "이름의 길이는 2 ~ 10글자 이어야 합니다.")
   @NotBlank(message = "이름은 필수입니다.")
   @ApiModelProperty(
       value = "이름",
       name = "name",
-      notes = "실명을 적어주세요.",
       example = "홍길동",
       required = true)
   private String name;
 
+  @Pattern(
+      regexp = "^01[0-9]{8,9}$",
+      message = "올바른 형식이 아닙니다.")
   @NotBlank(message = "전화번호는 필수입니다.")
   @ApiModelProperty(
       value = "전화번호",
       name = "phoneNumber",
-      notes = "전화번호를 적어주세요.",
-      example = "01012345678",
+      example = "01000000000",
       required = true)
   private String phoneNumber;
 
@@ -36,9 +40,40 @@ public class ReqSignUp {
   @ApiModelProperty(
       value = "이메일",
       name = "email",
-      notes = "이메일을 적어주세요.",
       example = "test@a.b",
       required = true)
   private String email;
+
+  @Size(
+      min = 2,
+      max = 12,
+      message = "닉네임의 길이는 2 ~ 12글자 이어야 합니다.")
+  @NotBlank(message = "닉네임은 필수입니다.")
+  @ApiModelProperty(
+      value = "닉네임",
+      name = "nickName",
+      example = "myNickName",
+      required = true)
+  private String nickName;
+
+  @Pattern(
+      regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$",
+      message = "비밀번호는 최소 8자에서 최대 16자, 최소 하나의 문자와 숫자, 특수문자를 포함해야 합니다.")
+  @ApiModelProperty(
+      value = "비밀번호",
+      name = "password",
+      example = "!mypassword486@")
+  private String password;
+
+  @Max(
+      value = 500,
+      message = "최대 500자 입니다.")
+  @ApiModelProperty(
+      value = "자기소개",
+      name = "introduction",
+      example = "안녕하세요.")
+  private String introduction;
+
+
 
 }
