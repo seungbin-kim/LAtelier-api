5e818daab8554e3669460ee84e8833deacec2520
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-06-08 14:40:46 +0900

    update: swagger 설정 변경
    - 설정변경

--- src/main/java/com/latelier/api/global/config/SwaggerConfig.java
+++ src/main/java/com/latelier/api/global/config/SwaggerConfig.java
@@ -2,10 +2,16 @@ package com.latelier.api.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,10 +20,37 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.OAS_30)
         .useDefaultResponseMessages(false)
+        .consumes(getConsumeContentTypes())
+        .produces(getProduceContentTypes())
+        .apiInfo(getApiInfo())
         .select()
         .apis(RequestHandlerSelectors
             .basePackage("com.latelier.api.domain"))
         .paths(PathSelectors.any())
+        .build();
+  }
+
+  private Set<String> getConsumeContentTypes() {
+    Set<String> consumes = new HashSet<>();
+    consumes.add("application/json;charset=UTF-8");
+    return consumes;
+  }
+
+  private Set<String> getProduceContentTypes() {
+    Set<String> produces = new HashSet<>();
+    produces.add("application/json;charset=UTF-8");
+    return produces;
+  }
+
+  private ApiInfo getApiInfo() {
+    return new ApiInfoBuilder()
+        .title("API")
+        .description("[Latelier] API")
+        .contact(new Contact(
+            "Latelier Swagger",
+            "https://www.notion.so/396ce7dc3ff04b8faeac3a6e8df8f609",
+            "seungbin.kim.dev@gmail.com"))
+        .version("1.0")
         .build();
   }
 
