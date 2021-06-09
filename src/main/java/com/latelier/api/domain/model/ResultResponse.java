88db89c106adec176d0ddcb1c7e63a208af0df04
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-06-09 14:02:16 +0900

    update: swagger model 어노테이션 작성
    - swagger ApiModel 어노테이션 작성

--- src/main/java/com/latelier/api/domain/model/ResultResponse.java
+++ src/main/java/com/latelier/api/domain/model/ResultResponse.java
@@ -1,12 +1,18 @@
 package com.latelier.api.domain.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@ApiModel("응답")
 public class ResultResponse<T> {
 
+  @ApiModelProperty(
+      value = "응답 데이터",
+      name = "content")
   private final T content;
 
 }
