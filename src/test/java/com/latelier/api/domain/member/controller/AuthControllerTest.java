9c3a23df38d52a8f46ebd2ea32e16939ecf0aa9f
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-06-20 22:57:35 +0900

    # <타입>: <제목> test: 로그인 테스트 작성
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    - 회원등록 후 로그인시 JWT 반환 테스트
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   add: 코드나 테스트, 예제 문서 등의 추가
    #   update: 원래도 정상이지만, 수정, 추가, 보완(주로 문서나 리소스, 라이브러리 등에 사용)
    #   remove: 코드의 삭제
    #   revise: 문서의 개정
    #   move: 코드의 이동
    #   rename: 이름 변경
    #   set: 변수 값 변경 등 작은 수정
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로(동사로 시작)
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- src/test/java/com/latelier/api/domain/member/controller/AuthControllerTest.java
+++ src/test/java/com/latelier/api/domain/member/controller/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package com.latelier.api.domain.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.latelier.api.domain.member.packet.request.ReqSignIn;
+import com.latelier.api.domain.member.packet.request.ReqSignUp;
 import com.latelier.api.domain.member.packet.request.ReqSmsAuthentication;
 import com.latelier.api.domain.member.packet.request.ReqSmsVerification;
 import com.latelier.api.domain.member.repository.SmsCertificationRepository;
@@ -17,8 +19,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
@@ -26,58 +31,110 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AuthControllerTest {
 
-  @Autowired
-  MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
 
-  @Autowired
-  ApplicationContext context;
+    @Autowired
+    ApplicationContext context;
 
-  @Autowired
-  ObjectMapper objectMapper;
+    @Autowired
+    ObjectMapper objectMapper;
 
-
-  @Test
-  @DisplayName("SMS_보내기")
-  void sendSms(@Value("${test.phoneNumber}") String phoneNumber) throws Exception {
-    // given
-    ReqSmsAuthentication req = new ReqSmsAuthentication();
-    ReflectionTestUtils.setField(req, "phoneNumber", phoneNumber);
-    String content = objectMapper.writeValueAsString(req);
-
-    // when
-    ResultActions perform = mockMvc.perform(post("/auth/sms")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(content));
-
-    // then
-    perform
-        .andExpect(status().isAccepted())
-        .andDo(print());
-  }
+    @Autowired
+    EntityManager em;
 
 
-  @Test
-  @DisplayName("SMS_인증확인")
-  void verifySms(@Value("${test.phoneNumber}") String phoneNumber) throws Exception {
-    // given
-    String certificationNumber = "123456";
-    SmsCertificationRepository repository = context.getBean(SmsCertificationRepository.class);
-    repository.saveSmsCertification(phoneNumber, Integer.parseInt(certificationNumber));
+    @Test
+    @DisplayName("SMS_보내기")
+    void sendSms(@Value("${test.phoneNumber}") String phoneNumber) throws Exception {
+        // given
+        ReqSmsAuthentication req = new ReqSmsAuthentication();
+        ReflectionTestUtils.setField(req, "phoneNumber", phoneNumber);
+        String content = objectMapper.writeValueAsString(req);
 
-    ReqSmsVerification req = new ReqSmsVerification();
-    ReflectionTestUtils.setField(req, "phoneNumber", phoneNumber);
-    ReflectionTestUtils.setField(req, "certificationNumber", certificationNumber);
-    String content = objectMapper.writeValueAsString(req);
+        // when
+        ResultActions perform = mockMvc.perform(post("/auth/sms")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
 
-    // when
-    ResultActions perform = mockMvc.perform(post("/auth/sms/verification")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(content));
+        // then
+        perform
+                .andExpect(status().isAccepted())
+                .andDo(print());
+    }
 
-    // then
-    perform
-        .andExpect(status().isOk())
-        .andDo(print());
-  }
+
+    @Test
+    @DisplayName("SMS_인증확인")
+    void verifySms(@Value("${test.phoneNumber}") String phoneNumber) throws Exception {
+        // given
+        String certificationNumber = "123456";
+        SmsCertificationRepository repository = context.getBean(SmsCertificationRepository.class);
+        repository.saveSmsCertification(phoneNumber, Integer.parseInt(certificationNumber));
+
+        ReqSmsVerification req = new ReqSmsVerification();
+        ReflectionTestUtils.setField(req, "phoneNumber", phoneNumber);
+        ReflectionTestUtils.setField(req, "certificationNumber", certificationNumber);
+        String content = objectMapper.writeValueAsString(req);
+
+        // when
+        ResultActions perform = mockMvc.perform(post("/auth/sms/verification")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        // then
+        perform
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+
+    @Test
+    @DisplayName("회원등록후_로그인_성공")
+    void signIn() throws Exception {
+        // given
+        registerRole();
+
+        String email = "test@a.b";
+        String password = "!mypassword486@";
+
+        ReqSignUp reqSignUp = new ReqSignUp();
+        ReflectionTestUtils.setField(reqSignUp, "name", "홍길동");
+        ReflectionTestUtils.setField(reqSignUp, "email", email);
+        ReflectionTestUtils.setField(reqSignUp, "phoneNumber", "01000000000");
+        ReflectionTestUtils.setField(reqSignUp, "password", password);
+        String signUpRequest = objectMapper.writeValueAsString(reqSignUp);
+
+        ReqSignIn reqSignIn = new ReqSignIn();
+        ReflectionTestUtils.setField(reqSignIn, "email", email);
+        ReflectionTestUtils.setField(reqSignIn, "password", password);
+        String content = objectMapper.writeValueAsString(reqSignIn);
+
+        // when
+        ResultActions signUp = mockMvc.perform(post("/api/members")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(signUpRequest));
+
+        ResultActions perform = mockMvc.perform(post("/auth/sign-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content));
+
+        // then
+        signUp
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        perform
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.token").isNotEmpty())
+                .andDo(print());
+    }
+
+
+    void registerRole() {
+        em.createNativeQuery("INSERT INTO authority (authority_name) VALUES ('ROLE_USER')").executeUpdate();
+        em.createNativeQuery("INSERT INTO authority (authority_name) VALUES ('ROLE_TEACHER')").executeUpdate();
+        em.createNativeQuery("INSERT INTO authority (authority_name) VALUES ('ROLE_ADMIN')").executeUpdate();
+    }
 
 }
