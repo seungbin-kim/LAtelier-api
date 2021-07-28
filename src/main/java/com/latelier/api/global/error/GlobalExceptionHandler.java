75a1f1da1fda0a847b1bbff8c2b3af510910dd87
Seungbin Kim <seungbin.kim.dev@gmail.com>
2021-07-28 18:58:31 +0900

    refactor: 어노테이션 변경
    - @ControllerAdvice -> @RestControllerAdvice 변경

--- src/main/java/com/latelier/api/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/latelier/api/global/error/GlobalExceptionHandler.java
@@ -9,14 +9,14 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import static org.springframework.http.HttpStatus.*;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
